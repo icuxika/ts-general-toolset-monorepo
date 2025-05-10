29172356cd07235ae04421c820200d98d5773112
icuxika <icuxika@outlook.com>
2025-05-10 23:10:37 +0800

    feat: add Tavily as a tool

--- apps/llm-mcp-rag/index.ts
+++ apps/llm-mcp-rag/index.ts
@@ -27,18 +27,28 @@ async function retrieveContext(input: string) {
     console.log("sum: ", add(1, 2));
 
     const currentDir = process.cwd();
-    const fileMCP = new MCPClient("mcp-server-filesystem", "npx", [
+    const filesystemMCP = new MCPClient("filesystem", "npx", [
         "-y",
         "@modelcontextprotocol/server-filesystem",
         currentDir,
     ]);
 
+    const tavilyMCP = new MCPClient("tavily-mcp", "npx", [
+        "-y",
+        "tavily-mcp@0.1.4",
+    ]);
+
     const outputPath = path.join(currentDir, "output");
 
-    const input = `从context中找到用户余克的相关信息，总结创作一个关于他的故事，把他的信息和故事保存到${outputPath}的story.txt文件中`;
+    const input = `从context中找到用户余克的相关信息，搜索一下用户所处地点天气情况，总结创作一个关于他的故事，把他的信息和故事保存到${outputPath}的story.txt文件中`;
     const context = await retrieveContext(input);
 
-    const agent = new Agent("deepseek-chat", [fileMCP], "", context);
+    const agent = new Agent(
+        "deepseek-chat",
+        [filesystemMCP, tavilyMCP],
+        "",
+        context
+    );
     await agent.initialize();
     const response = await agent.invoke(input);
     console.log(response);
